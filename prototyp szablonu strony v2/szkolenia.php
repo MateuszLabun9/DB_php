100afc6620988a6915fa4da56d4460e0a707ae76
Paweł Pawlik <pafcio333@gmail.com>
2020-11-04 22:30:43 +0000

    Upload New File

---
+++ prototyp szablonu strony v2/szkolenia.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+    <head>
+        <link rel="stylesheet" href="style/style_glowny.css" type="text/css" />
+    </head>
+    <body>
+      <h1>System rejestracji podań osób starających się o pracę</h1>
+     <div id="container">  
+        <form action="szkolenia.php" method="post">
+            
+            <p class="test">Etap 4: Przebyte szkolenia<br></p>
+            <input type="text" name="nazwa_szkolenia" placeholder="Nazwa szkolenia"><br>
+            <input type="text" name="rok_rozp_s" placeholder=" Rok rozpoczęcia"><br>
+            <br>
+            
+           
+        
+        </form>
+                   <a href=wymagane_dokumenty.php style="text-decoration: none;"><input  type="submit" value="DALEJ"></a> 
+        </div>
+    </body>
+</html>
