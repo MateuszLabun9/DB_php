fcd3cc9fa714f4d1e084740fb1a5e837277dd9e0
Paweł Pawlik <pafcio333@gmail.com>
2020-11-04 22:29:23 +0000

    Upload New File

---
+++ prototyp szablonu strony v2/wymagane_dokumenty.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+    <head>
+        <link rel="stylesheet" href="style/style_glowny.css" type="text/css" />
+        <script src="js/script.js"></script>
+    </head>
+    <body>
+      <h1>System rejestracji podań osób starających się o pracę</h1>
+     <div id="container">  
+          <form action="wymagane_dokumenty.php" method="post" enctype="multipart/form-data">
+         
+              
+              <input type="file" id="label" style="visibility: hidden;">
+              <label onclick="tekst()" for="label"> DODAJ PLIK</label>  
+              <span  id="file-chosen"></span>
+              <br>
+              <!-- tutaj trzeba dodać php by przesłać plik do sql -->
+              <input type="submit" name="submit" value="PRZEŚLIJ" >
+              <br>
+              
+              
+         </form>
+                    <a href=finalizacja.php style="text-decoration: none;"><input  type="submit" value="DALEJ"></a> 
+        </div>
+         
+    </body>
+</html>
