4771ea3c92c99cf2498d7f53b4f67027f12235dc
wikiber <wikasabernat@gmail.com>
2020-11-06 17:36:48 +0100

    wersja 3

---
+++ index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="utf-8" />
+    <title>Logowanie do systemu rewizji CV</title>
+    <link rel="stylesheet" href="style/logowanie_style.css">
+</head>
+    <body>
+        <h2>System rejestracji podań osób starających się o pracę</h2>
+        <div id="container">
+        <form action="logowanie.php" method="post">
+           <br>
+            
+            <h1>Logowanie</h1>
+            
+            <!-- blok loginu --> 
+             <input type="text" name="nazwa_uzytkownika" placeholder="login" onfocus="this.placeholder=''" onblur="this.placeholder='login'"><br>
+            
+            <!-- blok hasła --> 
+            <input type="password" name="haslo" placeholder="hasło"
+                   onfocus="this.placeholder=''" onblur="this.placeholder='hasło'"><br>
+            
+            <!-- blok logowania -->
+            <input type="submit" value="LOGIN"><br>
+             
+        </form>
+           <p><a href="rejestracja.php" >Nie masz jeszcze konta? Zarejestruj się!</a></p>
+            
+            <?php
+            
+            if(isset($_GET['alert'])){
+                if($_GET['alert']==1) echo "<p>Proszę poprawnie uzupełnić dane logowania!</p>";
+            }
+            
+            ?>
+    </div>
+    </body>
+    </html>
