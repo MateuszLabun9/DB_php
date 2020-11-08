b5f25509fec1ddde020b6161db734bd3d1469c64
themikyzeke <mikolaj.poppe@gmail.com>
2020-11-08 13:33:38 +0000

    Wersja 4 (jak wyżej) + pomoc kontekstowa

--- umiejetnosci.php
+++ umiejetnosci.php
@@ -20,6 +20,9 @@
          </form>
                   <a href=szkolenia.php style="text-decoration: none;"><input  type="submit" value="DALEJ"></a>  
          <!-- WYWALIĆ TE INPUTY Z ODNOŚNIKÓW TO JEST USELESS!!!!!!! DO IT EXACTLY LIKE petent.php!!!!!!!! -->
+                <div class="tooltip">Pomoc
+             <span class="tooltiptext">Wypisz wszystkie swoje umiejętności oraz ich poziom. Na przykład: język angielski, B2</span>
+         </div>
         </div>
     </body>
 </html>
