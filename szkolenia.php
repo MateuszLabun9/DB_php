fb9902301eef38b83d61c33e585a28115e2ba1d9
themikyzeke <mikolaj.poppe@gmail.com>
2020-11-08 13:33:53 +0000

    Wersja 4 (jak wyżej) + pomoc kontekstowa

--- szkolenia.php
+++ szkolenia.php
@@ -18,6 +18,9 @@
         </form>
                    <a href=wymagane_dokumenty.php style="text-decoration: none;"><input  type="submit" value="DALEJ"></a> 
          <!-- WYWALIĆ TE INPUTY Z ODNOŚNIKÓW TO JEST USELESS!!!!!!! DO IT EXACTLY LIKE petent.php!!!!!!!! -->
+        <div class="tooltip">Pomoc
+             <span class="tooltiptext">Wypisz wszystkie szkolenia które odbyłeś, uwzględnij rok rozpoczęcia.</span>
+         </div>
         </div>
     </body>
 </html>
