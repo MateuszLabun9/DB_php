674d80c25ffef68b3b4bcd1035a7ec105785eaba
themikyzeke <mikolaj.poppe@gmail.com>
2020-11-08 13:32:57 +0000

    Wersja 4 (jak wyżej) + pomoc kontekstowa

--- wyksztalcenie.php
+++ wyksztalcenie.php
@@ -20,6 +20,11 @@
         </form>
           <a href=doswiadczenie.php style="text-decoration: none;"><input  type="submit" value="DALEJ"></a>
          <!-- WYWALIĆ TE INPUTY Z ODNOŚNIKÓW TO JEST USELESS!!!!!!! DO IT EXACTLY LIKE petent.php!!!!!!!! -->
+         
+         <div class="tooltip">Pomoc
+             <span class="tooltiptext">Wpisz poziom swojego wykształcenia, dokładną nazwę szkoły oraz lata w których się uczyłeś.</span>
+         </div>
+         
         </div>
     </body>
 </html>
