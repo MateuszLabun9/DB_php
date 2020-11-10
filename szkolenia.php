a8144fe67a544ae9ef8503fc82856053b302ecbd
themikyzeke <mikolaj.poppe@gmail.com>
2020-11-10 16:58:27 +0000

    zmiana szkolenia na podstawie wyksztalcenia

--- szkolenia.php
+++ szkolenia.php
@@ -1,22 +1,29 @@
-<!doctype html>
-<html>
-    <head>
-        <link rel="stylesheet" href="style/style_glowny.css" type="text/css" />
-    </head>
-    <body>
-      <h1>System rejestracji podań osób starających się o pracę</h1>
-     <div id="container">  
-        <form action="szkolenia.php" method="post">
-            
-            <p class="test">Etap 4: Przebyte szkolenia<br></p>
-            <input type="text" name="nazwa_szkolenia" placeholder="Nazwa szkolenia"><br>
-            <input type="text" name="rok_rozp_s" placeholder=" Rok rozpoczęcia"><br>
-            <br>
-            
-           
+<?php
+class Szkolenia{
+    public $nazwa_szkolenia;
+    public $id_uzytkownika;
+    public $rok_rozp_s;
+    public function dodajSzkolenia($nazwa_szkolenia, $id_uzytownika, $rok_rozp_s){
+        require_once "connect.php";
+        $this->nazwa_szkolenia = $nazwa_szkolenia;
+        $this->id_uzytkownika = $id_uzytkownika;
+        $this->rok_rozp_s = $rok_rozp_s;
         
-        </form>
-                   <a href=wymagane_dokumenty.php style="text-decoration: none;"><input  type="submit" value="DALEJ"></a> 
-        </div>
-    </body>
-</html>
+        $conn = @new mysqli($host, $db_user, $db_password, $db_name);
+        
+        if($conn->connect_errno!=0){
+            header("Location: rejestracja.php?alert=4");//nie laczy z baza
+        }
+        else{
+            $sql = "INSERT INTO szkolenia (klucz_szkolenia, id_uzytkownika, nazwa_szkolenia, rok_rozp_s) VALUES (NULL, '".$id_uzytownika."', '".$nazwa_szkolenia."', '".$rok_rozp_s."');";
+            if($conn->query($sql) === TRUE){
+                header("Location: form_szkolenia.php");//dodano wyksztalcenie
+            }
+            else{
+                header("Location: form_szkolenia.php?alert=3");//blad obslugi zapytania
+            }
+        }
+        
+    }
+}
+?>
