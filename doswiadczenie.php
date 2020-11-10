e08b6e13e2d7727acc582428df34d4ff7c3ef2b9
themikyzeke <mikolaj.poppe@gmail.com>
2020-11-10 17:00:36 +0000

    Zmiana doswiadczneie na podstawie wyksztalcenie

--- doswiadczenie.php
+++ doswiadczenie.php
@@ -1,25 +1,33 @@
-<!doctype html>
-<html>
-    <head>
-        <link rel="stylesheet" href="style/style_glowny.css" type="text/css" />
-    </head>
-    <body>
-      <h1>System rejestracji podań osób starających się o pracę</h1>
-     <div id="container">  
-         
-        <form action="doswiadczenie.php" method="post">
-            
-            <p class="test">    Etap 2: Doświadczenie zawodowe<br></p>
-            <input type="text" name="nazwa_firmy" placeholder="Nazwa Firmy"><br>
-            <input type="text" name="stanowisko" placeholder="Stanowisko"><br>
-            <input type="number" name="rok_rozp_d" placeholder=" Rok rozpoczęcia"><br>
-            <input type="number" name="rok_rozp_d" placeholder="Rok zakończenia"><br>
-            <br>
-            <br>
-            
+<?php
+class Doswiadczenie{
+    public $nazwa_firmy;
+    public $id_uzytkownika;
+    public $stanowisko;
+    public $rok_rozp_d;
+    public $rok_zak_d;
+    public function dodajDoswiadczenie($nazwa_firmy, $id_uzytownika, $stanowisko, $rok_rozp_d, $rok_zak_d){
+        require_once "connect.php";
+        $this->nazwa_firmy = $nazwa_firmy;
+        $this->id_uzytkownika = $id_uzytkownika;
+        $this->stanowisko = $stanowisko;
+        $this->rok_rozp_d = $rok_rozp_d;
+        $this->rok_zak_d = $rok_zak_d;
         
-        </form>
-               <a href=umiejetnosci.php style="text-decoration: none;"><input  type="submit" value="DALEJ"></a>    
-        </div>
-    </body>
-</html>
+        $conn = @new mysqli($host, $db_user, $db_password, $db_name);
+        
+        if($conn->connect_errno!=0){
+            header("Location: rejestracja.php?alert=4");//nie laczy z baza
+        }
+        else{
+            $sql = "INSERT INTO doswiadczenie (klucz_doswiadczenie, id_uzytkownika, nazwa_firmy, stanowisko, rok_rozpoczecia, rok_zakonczenia) VALUES (NULL, '".$id_uzytownika."', '".$nazwa_firmy."', '".$stanowisko."', '".$rok_rozp_d."', '".$rok_zak_d."');";
+            if($conn->query($sql) === TRUE){
+                header("Location: form_doswiadczenie.php");//dodano wyksztalcenie
+            }
+            else{
+                header("Location: form_doswiadczenie.php?alert=3");//blad obslugi zapytania
+            }
+        }
+        
+    }
+}
+?>
